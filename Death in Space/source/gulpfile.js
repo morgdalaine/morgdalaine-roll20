500e5534db3a1b7d9e30b59023aa3a6a2e9103e1
Morgdalaine Yaho <morgdalaine@gmail.com>
2021-05-19 13:31:36 -0500

    Update packages to reduce vulnerabilities
    Add repository and funding to package.json

--- Death in Space/source/gulpfile.js
+++ Death in Space/source/gulpfile.js
@@ -1,9 +1,6 @@
-const pug = require('gulp-pug');
-const stylus = require('gulp-stylus');
 const gulp = require('gulp');
+const pug = require('gulp-pug-3');
 const sass = require('gulp-sass');
-const prefix = require('gulp-autoprefixer');
-const concat = require('gulp-concat');
 
 gulp.task('sass', () => {
   return gulp.src('./app/DeathInSpace.scss')
@@ -12,18 +9,6 @@ gulp.task('sass', () => {
       }).on('error', sass.logError))
     .pipe(gulp.dest('../'))
 })
-
-// gulp.task('sass', function () {
-//   return gulp.src(['./app/**/index.scss'])
-//     .pipe(sass({
-//       outputStyle: 'expanded'
-//     }).on('error', sass.logError))
-//     .pipe(prefix({
-//       cascade: true
-//     }))
-//     .pipe(concat('DeathInSpace.css'))
-//     .pipe(gulp.dest('../'))
-// })
 
 gulp.task('html', function () {
   return gulp.src('./app/DeathInSpace.pug')
@@ -35,12 +20,10 @@ gulp.task('html', function () {
 })
 
 gulp.task('watch', gulp.series(['sass', 'html'], () => {
-  // gulp.watch('./app/**/*.styl', gulp.series(['css']))
   gulp.watch('./app/**/*.scss', gulp.series(['sass']))
   gulp.watch(['./app/**/*.pug','./app/**/*.js'], gulp.series(['html']))
 }))
 
 gulp.task('build', gulp.series([
-  // 'css',
   'sass',
   'html']))
